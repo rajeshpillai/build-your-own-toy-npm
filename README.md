7e80ea045909adee38dae535f4f11f8a97c363db
rajesh.algorisys <rajesh.algorisys@ext.idfy.com>
2023-04-07 10:03:57 +0530

    Update toy-package.json.  TODO: Fix uninstall removing package entry from json file

---
+++ README.md
@@ -0,0 +1,9 @@
+# Tiny npm 
+A learning project to explore how npm works
+
+
+# Usage
+
+```js
+node tiny-npm.js <install/uninstall> <package-name>
+```
--- tiny-npm.js
+++ tiny-npm.js
@@ -22,68 +22,111 @@ async function downloadPackage(packageName, version) {
 
   const tarballUrl = metadata.versions[resolvedVersion].dist.tarball;
   const { data } = await axios.get(tarballUrl, { responseType: "stream" });
+
+  // Attach the resolved version to the returned stream
+  data.resolvedVersion = resolvedVersion;
+
   return data;
 }
 
 
-async function installPackageXX(packageName, version) {
-  const packagePath = path.join(__dirname, "toy_node_modules", packageName);
-  console.log("Package Path: ", packagePath);
-
-  fs.mkdirSync(packagePath, { recursive: true });
-
-  const packageStream = await downloadPackage(packageName, version);
-  const writeStream = fs.createWriteStream(
-    path.join(packagePath, `${packageName}-${version}.tgz`)
-  );
-
-  packageStream.pipe(writeStream);
-}
-
-async function installPackage(packageName, version) {
+async function installPackage(packageName, version, isDevDependency = false) {
   const packagePath = path.join(__dirname, "toy_node_modules", packageName);
   fs.mkdirSync(packagePath, { recursive: true });
 
   const packageStream = await downloadPackage(packageName, version);
+  // Update the version variable with the resolved version
+  version = packageStream.resolvedVersion;
+
   const writeStream = fs.createWriteStream(
     path.join(packagePath, `${packageName}-${version}.tgz`)
   );
 
   packageStream.pipe(writeStream);
 
-  writeStream.on("finish", () => {
-    tar.x({
-      file: path.join(packagePath, `${packageName}-${version}.tgz`),
-      cwd: packagePath,
-      strip: 1,
-    }).then(() => {
-      // Remove the tarball after extraction
-      fs.unlinkSync(path.join(packagePath, `${packageName}-${version}.tgz`));
+  await new Promise((resolve) => {
+    writeStream.on("finish", () => {
+      tar.x({
+        file: path.join(packagePath, `${packageName}-${version}.tgz`),
+        cwd: packagePath,
+        strip: 1,
+      }).then(() => {
+        // Remove the tarball after extraction
+        fs.unlinkSync(path.join(packagePath, `${packageName}-${version}.tgz`));
+        // Update toy-package.json
+        updateToyPackageJson(packageName, version, isDevDependency);
+        resolve();
+      });
     });
   });
 }
+
+
 
 async function uninstallPackage(packageName) {
   const packagePath = path.join(__dirname, "toy_node_modules", packageName);
   fs.rmSync(packagePath, { recursive: true, force: true });
 }
 
+
+
+// Update toy-package.json
+function updateToyPackageJson(packageName, version, isDevDependency = false) {
+  const toyPackagePath = path.join(__dirname, "toy-package.json");
+
+  let toyPackageJson = {
+    name: "toy-npm",
+    version: "1.0.0",
+    description: "",
+    main: "index.js",
+    dependencies: {},
+    devDependencies: {},
+  };
+
+  if (fs.existsSync(toyPackagePath)) {
+    toyPackageJson = JSON.parse(fs.readFileSync(toyPackagePath, "utf8"));
+  }
+
+  if (isDevDependency) {
+    toyPackageJson.devDependencies[packageName] = version;
+  } else {
+    toyPackageJson.dependencies[packageName] = version;
+  }
+
+  fs.writeFileSync(toyPackagePath, JSON.stringify(toyPackageJson, null, 2));
+}
+
 async function main() {
-  const [action, packageName, version] = process.argv.slice(2);
+  const [action, packageName, ...restArgs] = process.argv.slice(2);
+
+  let version;
+  let saveOption;
+  let isDevDependency = false;
+
+  restArgs.forEach((arg) => {
+    if (arg.startsWith("--save")) {
+      saveOption = arg;
+    } else {
+      version = arg;
+    }
+  });
+
+  isDevDependency = saveOption === "--save-dev";
 
   switch (action) {
     case "install":
-      await installPackage(packageName, version);
-      console.log(`Installed ${packageName}@${version}`);
+      await installPackage(packageName, version, isDevDependency);
+      console.log(`Installed ${packageName}@${version || "latest"}`);
       break;
-    case "uninstall":
-      uninstallPackage(packageName);
-      console.log(`Uninstalled ${packageName}`);
-      break;
-    default:
-      console.error("Invalid action. Use 'install' or 'uninstall'.");
+      case "uninstall":
+        uninstallPackage(packageName);
+        console.log(`Uninstalled ${packageName}`);
+        break;
+      default:
+        console.error("Invalid action. Use 'install' or 'uninstall'.");
   }
 }
+
 
 main().catch((error) => {
   console.error(`Error: ${error.message}`);
